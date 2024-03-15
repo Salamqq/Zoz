e1e2fdf98b778bde3360efdf38a9fe211929f763
Salamqq <163534782+Salamqq@users.noreply.github.com>
2024-03-16 00:23:18 +0300

    Delete strings/filters.py

--- strings/filters.py
+++
@@ -1,9 +0,0 @@
-from typing import List, Union
-
-from pyrogram import filters
-
-
-other_filters = filters.group
-
-def command(commands: Union[str, List[str]]):
-    return filters.command(commands, "")
